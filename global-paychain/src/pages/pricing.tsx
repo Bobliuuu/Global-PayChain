18183481dc292fdd75c2814a7fb1a249fe0fca0d
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2023-06-18 11:12:11 -0400

    More typos

--- global-paychain/src/pages/pricing.tsx
+++ global-paychain/src/pages/pricing.tsx
@@ -9,7 +9,6 @@ const Pricing = () => {
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="max-w-3xl p-8 bg-white rounded shadow-lg">
         <h1 className="text-3xl font-bold mb-8">Pricing Models for Global PayChain</h1>
-
         <div className="grid grid-cols-3 gap-8">
           <div className="border border-gray-300 rounded p-6">
             <h2 className="text-xl font-bold mb-4">Free Tier (0$/month)</h2>
@@ -24,7 +23,7 @@ const Pricing = () => {
 
           <div className="border border-gray-300 rounded p-6">
             <h2 className="text-xl font-bold mb-4">Subscription Tier (10$/month)</h2>
-              <p className="mb-4">Up to 1000 transactions a month for only 10$</p>
+              <p className="mb-4">Up to 1000 transactions per month for only 10$</p>
             <p className="mb-4">Unlimited wallets and token buy/swap</p>
             <button
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
